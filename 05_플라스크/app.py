0a685389322b571d6e0c567796bfa7fcafe77862
ryuinhwan9613-cell <ryuinhwan9613@gmail.com>
2026-04-02 21:05:12 +0900

    Add files via upload

--- 05_플라스크/app.py
+++ 05_플라스크/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.heml")
 
 
 @app.route("/hello")
@@ -16,7 +16,7 @@ def profile(userId):
     return f"{userId}\" profile"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
 
 
 
